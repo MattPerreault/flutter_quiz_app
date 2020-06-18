0cf5a2c2e7cf2d8a9669d184f6f153866592cb36
MattPerreault <mattperreault.chew@gmail.com>
2020-06-17 21:00:01 -0600

    Format results

--- flutter_quiz_app/lib/result.dart
+++ flutter_quiz_app/lib/result.dart
@@ -1,10 +1,33 @@
 import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
+  final int totalScore;
+  Result(this.totalScore);
+
+  String get resultPhrase {
+    var resultText;
+    if (totalScore <= 8) {
+      resultText = 'You are awesome!';
+    } else if (totalScore <= 12){
+      resultText = 'Ehh, I would have chosen some different answers.';
+    } else if( totalScore <= 16){
+      resultText = 'You need help.';
+    }
+      
+    return resultText; 
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text('You answered all the questions'),
+      child: Text (
+        resultPhrase,
+        style: TextStyle(
+          fontSize: 36,
+          fontWeight: FontWeight.bold,
+        ),
+        textAlign: TextAlign.center,
+      ),
     );
   }
 }
