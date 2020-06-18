e6ca83cea0ba3065bc167c17b54de7c36a738337
MattPerreault <mattperreault.chew@gmail.com>
2020-06-17 19:53:30 -0600

    Add scores to answers, and total score property, pass method reference to mapping.

--- flutter_quiz_app/lib/main.dart
+++ flutter_quiz_app/lib/main.dart
@@ -14,22 +14,42 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   var _questionIndex = 0;
+  var _totalScore = 0;
   final _questions = const [
     {
       'questionText': 'What\'s your favorite color?',
-      'answers': ['Red', 'Orange', 'Yellow', 'Green', 'Blue', 'Purple']
+      'answers': [
+        {'text': 'Red', 'score': 10},
+        {'text': 'Orange', 'score': 9},
+        {'text': 'Yellow', 'score': 7},
+        {'text': 'Green', 'score': 1},
+        {'text': 'Blue', 'score': 3},
+        {'text': 'Purple', 'score': 2}
+      ]
     },
     {
       'questionText': 'What\'s your favorite animal?',
-      'answers': ['Dog', 'Cat', 'Bird', 'Turtle']
+      'answers': [
+        {'text': 'Dog', 'score': 1},
+        {'text': 'Cat', 'score': 3},
+        {'text': 'Bird', 'score': 4},
+        {'text': 'Turtle', 'score': 2}
+      ]
     },
     {
       'questionText': 'What\'s your favorite city?',
-      'answers': ['Los Angeles', 'Denver', 'Austin', 'Portland']
+      'answers': [
+        {'text': 'Los Angeles', 'score': 4},
+        {'text': 'Denver', 'score': 1},
+        {'text': 'Austin', 'score': 2},
+        {'text': 'Portland', 'score': 3}
+      ]
     }
   ];
 
-  void _answerQuestion() {
+  void _answerQuestion(int score) {
+    _totalScore += score;
+
     setState(() {
       _questionIndex++;
     });
